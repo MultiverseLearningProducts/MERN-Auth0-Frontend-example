e0e6707a29c92b40a5992fc9b185f8e61fefe9dc
Bernard Mordan <bernardmordan@gmail.com>
2022-07-05 14:30:32 +0100

    basic react routing example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import { Auth0Provider } from "@auth0/auth0-react"
 import { MainView } from "./MainView"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Landing, Bookings, Menus, Profile } from './Pages'
+
+const someUserId = 1234567
+
 export function App() {
     return (
         <Auth0Provider 
             domain="https://dev-7954hoz9.eu.auth0.com"
             clientId="kaNidIpBNzoVOBWRj5AP2uZWb0YlRYzQ"
             redirectUri={window.location.origin}>
-            <MainView />
+            <BrowserRouter>
+                <nav style={{display: 'flex'}}>
+                    <h1>MERN Restaurants</h1>
+                    <Link to="/">&nbsp;Home</Link>
+                    <Link to="/bookings">&nbsp;Bookings</Link>
+                    <Link to="/menus">&nbsp;Menus</Link>
+                    <Link to={`/users/${someUserId}`}>Your Profile</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/bookings" element={<Bookings />} />
+                    <Route path="/menus" element={<Menus />} />
+                    <Route path="/users/:id" element={<Profile />} />
+                </Routes>
+            </BrowserRouter>
         </Auth0Provider>
     )
 }
---
+++ src/Pages.js
@@ -0,0 +1,35 @@
+import { useParams, useNavigate } from "react-router-dom"
+
+export function Menus() {
+    return (
+        <section>
+            <h1>Menus page</h1>
+        </section>
+    )
+}
+export function Bookings() {
+    const navigate = useNavigate()
+
+    return (
+        <section>
+            <h1>Bookings page</h1>
+            <button onClick={() => navigate("/", {replace: true})}>back</button>
+        </section>
+    )
+}
+export function Landing() {
+    return (
+        <section>
+            <h1>Landing page</h1>
+        </section>
+    )
+}
+export function Profile() {
+    const {id} = useParams()
+    return (
+        <section>
+            <h1>User with id of {id}</h1>
+        </section>
+    )
+
+}
